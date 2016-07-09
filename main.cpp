f09a3c993a672f677659233cef695d7f633a8dad
liweibin <wibinly@qq.com>
2016-07-09 10:28:59 +0800

    modify main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char *argv[])
     stdout = freopen("./../log/output.log", "w+", stdout);
 
     std::cout << "there is main function...\n";
-
+    
+    std::cout << "declation TestA a..\n";
+    
     TestA a;
     
     a.ShowInfo();
